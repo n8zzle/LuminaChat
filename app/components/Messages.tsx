418e60e38671c747f227bae0372d0db65092fe32
n8zzle <playwithn8zzle@gmail.com>
2023-08-20 14:23:59 +0200

    created Message component

---
+++ app/components/Messages.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import { IconButton } from "@mui/material";
+const Messages = () => {
+  return (
+    <div className="login-bg w-full h-full flex">
+      <div className="my-auto mx-auto w-[90%] h-[90%] glassmorphism rounded-lg p-5">
+        <div className="h-[95%]">Chat</div>
+        <div className="h-[5%] flex justify-between items-center bg-gray-50 rounded-full p-3">
+          <div className="w-[95%] ">
+            <input
+              type="text"
+              placeholder={"Message..."}
+              className="w-full bg-transparent outline-none"
+            />
+          </div>
+          <div className="w-[5%] text-center ">
+            <IconButton>
+              <TelegramIcon className="text-black" />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
