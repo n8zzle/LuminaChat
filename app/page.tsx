a979187ed7e46df9a1b2ab08ef0159811e1c8610
n8zzle <playwithn8zzle@gmail.com>
2023-08-20 14:23:21 +0200

    First time using lazygit, whant to check how it works

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className=" w-screen h-screen">
-      <div className="p-10 ">
-        <h1>Welcome to the Chat-app</h1>
+      <div className="login-bg w-full h-full flex ">
+        <div className="my-auto mx-auto w-[90%] h-[90%] glassmorphism rounded-lg p-5">
+          <h1 className="text-4xl font-extrabold text-center">
+            Welcome to Lumina-Chat
+          </h1>
+        </div>
       </div>
     </main>
   );
